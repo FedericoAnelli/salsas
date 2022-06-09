e667021f253f8ece692c5bcb2b8446811c06a5da
Federico Anelli <federicoanelli@gmail.com>
2022-06-09 17:17:45 -0300

    Fixes

--- js/productosLoad.js
+++ js/productosLoad.js
@@ -31,12 +31,12 @@ class Producto{
     }
 }
 
-// Carga inicial de productos
+/* Carga inicial de productos
 agregarProducto("/media/fotosSalsas/tabasco.webp", "Tabasco", "Originaria de Louisiana, Estados Unidos, se comercializa desde el siglo XIX, exactamente creada en 1868 por la empresa familiar McIlhenny Company. La receta original consistía en una mezcla de chiles tabasco rojo, agua, sal y vinagre que se maceraba en barricas de roble para que adquieran sabor. Tabasco Garlic. Esta salsa no lleva chile. La base picante de esta salsa es el ajo. El ajo no contiene capsaicina y el picor que produce es debido a otro tipo de sustancias. Es por tanto única en la gama de Tabasco al no contener ningún tipo de chile. Contiene una cantidad equivalente de picante a una horquilla de entre 1.200 y 2.400 SHU.", 1, "Ajo", 850, 50);
 agregarProducto("/media/fotosSalsas/yucatecoBlack.webp", "Yucateco Black", "Esta salsa negra es el último lanzamiento de la línea de salsas habaneras, su sabor original ahumado y único se originan al asar chiles habaneros a altas temperaturas. Si te gusta el chipotle (jalapeño ahumado) no podés dejar de probar Habaneros Ahumados. <br> Es ideal para carnes y verduras a la parrilla o añadir un toque de sabor a la leña a sopas o guisos.", 8, "Habanero", 899, 50);
 agregarProducto("/media/fotosSalsas/yucatecoChipotle.webp", "Yucateco Chipotle", "Originaria de Louisiana, Estados Unidos, se comercializa desde el siglo XIX, exactamente creada en 1868 por la empresa familiar McIlhenny Company. La receta original consistía en una mezcla de chiles tabasco rojo, agua, sal y vinagre que se maceraba en barricas de roble para que adquieran sabor. Tabasco Garlic. Esta salsa no lleva chile. La base picante de esta salsa es el ajo. El ajo no contiene capsaicina y el picor que produce es debido a otro tipo de sustancias. Es por tanto única en la gama de Tabasco al no contener ningún tipo de chile. Contiene una cantidad equivalente de picante a una horquilla de entre 1.200 y 2.400 SHU.", 1, "Ajo", 850, 50);
 agregarProducto("/media/fotosSalsas/yucatecoVerde.webp", "Yucateco Jalapeño", "Para los fanáticos del chipotle en todas variedades, la famosa salsa Chipotle de Yucateco. Con el clásico sabor ahumado de todos los chipotles <br> Elaborada a base de chiles chipotles ahumados al natural y jarabe de maíz. Lo que ha dado como resultado una mezcla de sabores picantes y agridulces que seguramente provocarán todos sus sentidos. <br> Usala para marinar unas fajitas de pollo o para preparar un delicioso dip para camarones, verduras y más.", 3, "Chipotle", 830, 50);
-
+*/
 
 // Genera IDs automaticamente
 function generadorDeID(nombreProducto)
